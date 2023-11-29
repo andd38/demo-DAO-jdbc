2bb304ba734dd65c0f4ad1af40726a74178334ab
andre patriarca <andrepatriarca4@gmail.com>
2023-11-29 14:22:11 -0300

    DAO Factory

---
+++ src/DAO/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package DAO.impl;
+
+import java.util.List;
+
+import DAO.SellerDAO;
+import entities.Seller;
+
+public class SellerDaoJDBC implements SellerDAO{
+
+	@Override
+	public void insert(Seller obj) {
+		
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		
+		return null;
+	}
+	
+}
