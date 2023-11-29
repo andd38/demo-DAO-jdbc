6eabc5d66f73d0fd6977d400244f8870a95a0c35
andre patriarca <andrepatriarca4@gmail.com>
2023-11-29 10:26:08 -0300

    Creating project and git repository

---
+++ src/db/DbIntergratyException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntergratyException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+	public DbIntergratyException(String msg) {
+		super(msg); 
+	}
+
+}
