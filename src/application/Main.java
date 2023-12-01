68712bd9170c380bb6a27ab5e6bf1aad5ac64e2b
andre patriarca <andrepatriarca4@gmail.com>
2023-12-01 14:33:34 -0300

    find all implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,11 +17,18 @@ public class Main {
 		
 		//Seller seller = sellerDao.findById(3);
 		//System.out.println(seller);
-		Department department = new Department(2,null);
-		System.out.println("===teste 2 : seller findBy id===");
-		List<Seller>list = sellerDao.findByDepartment(department);
-		for (Seller seller : list) {
-			System.out.println(seller);
+		/*
+		 * Department department = new Department(2,null);
+		 * System.out.println("===teste 2 : seller findBy id==="); List<Seller>list =
+		 * sellerDao.findByDepartment(department); for (Seller seller : list) {
+		 * System.out.println(seller); }
+		 */
+		
+		
+		System.out.println("===teste 3 : seller findBy id===");
+		List<Seller>list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
 		}
 	}
 
