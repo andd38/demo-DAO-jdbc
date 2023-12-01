25b7d9e73fbdb68b0bf4519fc6f1f82e212a3e97
andre patriarca <andrepatriarca4@gmail.com>
2023-12-01 09:47:00 -0300

    Reusing intantion

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		SellerDAO sellerDao= DAOFactory.createSellerDao();
+		
+		System.out.println("===teste 1 : seller findBy id===");
+		
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
